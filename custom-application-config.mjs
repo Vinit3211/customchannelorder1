4982955f001161f6b71fb7716439eeb77bfdc769
Vinit3211 <vinit.kumar@borngroup.com>
2022-10-27 14:56:37 +0530

    first commit

---
+++ custom-application-config.mjs
@@ -0,0 +1,39 @@
+import { PERMISSIONS, entryPointUriPath } from './src/constants';
+
+/**
+ * @type {import('@commercetools-frontend/application-config').ConfigOptions}
+ */
+const config = {
+  name: 'Custom Applications',
+  entryPointUriPath,
+  cloudIdentifier: 'gcp-eu',
+  env: {
+    development: {
+      initialProjectKey: 'global-new',
+    },
+    production: {
+      applicationId: 'cl9qo0xpl01gay2013cl2xwvw',
+      url: 'https://global-new.vercel.app',
+    },
+  },
+  oAuthScopes: {
+    view: ['view_products'],
+    manage: ['manage_products'],
+  },
+  icon: '${path:@commercetools-frontend/assets/application-icons/rocket.svg}',
+  mainMenuLink: {
+    defaultLabel: 'Template starter',
+    labelAllLocales: [],
+    permissions: [PERMISSIONS.View],
+  },
+  submenuLinks: [
+    {
+      uriPath: 'channels',
+      defaultLabel: 'Channels',
+      labelAllLocales: [],
+      permissions: [PERMISSIONS.View],
+    },
+  ],
+};
+
+export default config;
